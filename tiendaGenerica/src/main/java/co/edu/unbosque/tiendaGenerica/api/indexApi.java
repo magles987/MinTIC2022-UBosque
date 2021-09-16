e2624ee09e000d208525ddeafc373efaf6df205f
magles987 <magles978@gmail.com>
2021-09-15 21:06:01 -0500

    documentacion

--- tiendaGenerica/src/main/java/co/edu/unbosque/tiendaGenerica/api/indexApi.java
+++ tiendaGenerica/src/main/java/co/edu/unbosque/tiendaGenerica/api/indexApi.java
@@ -3,10 +3,15 @@ package co.edu.unbosque.tiendaGenerica.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+/**
+ * controlador generico
+ * */
 @Controller 
 public class indexApi {
 
+	/**
+	 * recibe la peticion inicial
+	 * */
 	@GetMapping()
 	public String descargarApi() {		
 		return "index";	
