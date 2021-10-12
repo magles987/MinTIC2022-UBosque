a080831fb468bc2e181594d261c6a73a23c39ed3
Brian León Hoyos <69739801+Brianleonhoyos@users.noreply.github.com>
2021-10-11 21:53:18 -0500

    Update IProductoDao.java

--- tiendaGenerica/src/main/java/co/edu/unbosque/tiendaGenerica/dao/IProductoDao.java
+++ tiendaGenerica/src/main/java/co/edu/unbosque/tiendaGenerica/dao/IProductoDao.java
@@ -1,3 +1,13 @@
 package co.edu.unbosque.tiendaGenerica.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.unbosque.tiendaGenerica.model.Producto;
+
+
+public interface IProductoDao extends JpaRepository<Producto, Long> {
+
+	
+}
